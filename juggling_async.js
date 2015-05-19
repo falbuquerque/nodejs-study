0cb83d05a4afa3cc9c08c952e9ad5100ca748e42
falbuquerque <albuquerque.felipe@gmail.com>
2015-05-18 23:14:03 -0300

    HTTP studies

---
+++ juggling_async.js
@@ -0,0 +1,21 @@
+var bl = require('bl')
+var http = require('http')
+var count = 0
+var results = []
+var calls = 3
+
+function print() {
+  for (var i = 0; i < calls; i++) console.log(results[i])
+}
+
+function get(index) {
+  http.get(process.argv[2 + index], function(response) {
+    response.pipe(bl(function(err, data) {
+      results[index] = data.toString()
+      count++
+      if (count == calls) print()
+    }))
+  })
+}
+
+for (var i = 0; i < calls; i++) get(i)
